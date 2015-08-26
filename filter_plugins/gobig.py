02d7791163e746cd4f3169ba3ddca4000ce4eb4e
Omar Padron <omar.padron@kitware.com>
2015-08-26 10:44:05 -0400

    rename host_list variable in zk_url filter

--- filter_plugins/gobig.py
+++ filter_plugins/gobig.py
@@ -141,16 +141,16 @@ class HostsFileFilterFunction(object):
 
         return result.getvalue()
 
-def zk_url(list, hostvars, net_interface, client_port):
+def zk_url(host_list, host_vars, net_interface, client_port):
     net_interface_key = "ansible_{}".format(net_interface)
     return "".join((
         "zk://",
         ":{},".format(str(client_port)).join(
-            hostvars[host]
-                    [net_interface_key]
-                    ["ipv4"]
-                    ["address"]
-            for host in list),
+            host_vars[host]
+                     [net_interface_key]
+                     ["ipv4"]
+                     ["address"]
+            for host in host_list),
         ":",
         str(client_port),
     ))
