1fef9aeb455e172852251d5b0a7837d311c3614a
Omar Padron <omar.padron@kitware.com>
2015-10-21 12:45:05 -0400

    rewrite girder-post-install script to remove superfluous functions

--- roles/girder-post-install/files/girder-post-install.py
+++ roles/girder-post-install/files/girder-post-install.py
@@ -9,28 +9,6 @@ from time import sleep
 from girder.constants import AssetstoreType
 from girder_client import GirderClient
 
-def rest(client, method, *args, **kwds):
-    method = getattr(client, method)
-    old_parameters = kwds.pop('parameters')
-    kwds['parameters'] = {}
-    kwds['parameters'].update(
-        (k, v[1:-1]) if (v.startswith('"') and v.endswith('"')) else (k, v)
-        for k, v in (
-            (key, json.dumps(value))
-            for key, value in old_parameters.items()))
-
-    print kwds['parameters']
-    return method(*args, **kwds)
-
-def post(client, *args, **kwds):
-    return rest(client, 'post', *args, **kwds)
-
-def put(client, *args, **kwds):
-    return rest(client, 'put', *args, **kwds)
-
-def get(client, *args, **kwds):
-    return rest(client, 'get', *args, **kwds)
-
 def register_and_authenticate(client, **kwds):
     username = kwds['login']
     password = kwds['password']
@@ -38,13 +16,11 @@ def register_and_authenticate(client, **kwds):
     try:
         client.authenticate(username, password)
     except:
-        post(client,
-             'user',
-             parameters=dict(login=username,
-                             password=password,
-                             email=kwds['email'],
-                             firstName=kwds['firstName'],
-                             lastName=kwds['lastName']))
+        client.post('user', parameters=dict(login=username,
+                                            password=password,
+                                            email=kwds['email'],
+                                            firstName=kwds['firstName'],
+                                            lastName=kwds['lastName']))
         client.authenticate(username, password)
 
 def find_assetstore(name):
@@ -52,9 +28,9 @@ def find_assetstore(name):
     limit = 50
     result = None
     while result is None:
-        assetstore_list = get(client,
-                              'assetstore',
-                              parameters=dict(limit=limit, offset=offset))
+        assetstore_list = client.get('assetstore',
+                                     parameters=dict(limit=str(limit),
+                                                     offset=str(offset)))
 
         if not assetstore_list:
             break
@@ -73,9 +49,9 @@ def find_collection(name):
     limit = 50
     result = None
     while result is None:
-        collection_list = get(client,
-                              'collection',
-                              parameters=dict(limit=limit, offset=offset))
+        collection_list = client.get('collection',
+                                     parameters=dict(limit=str(limit),
+                                                     offset=str(offset)))
 
         if not collection_list:
             break
@@ -115,54 +91,50 @@ register_and_authenticate(client,
 local_assetstore_name = 'local'
 
 if find_assetstore(local_assetstore_name) is None:
-    post(client,
-         'assetstore',
-         parameters=dict(name=local_assetstore_name,
-                         type=AssetstoreType.FILESYSTEM,
-                         root=os.path.join(args.data_root,
-                                           'assetstores',
-                                           'local'),
-                         readOnly=False))
+    client.post('assetstore',
+                parameters=dict(name=local_assetstore_name,
+                                type=str(AssetstoreType.FILESYSTEM),
+                                root=os.path.join(args.data_root,
+                                                  'assetstores',
+                                                  'local'),
+                                readOnly="false"))
 
-put(client,
-    'system/plugins',
-    parameters=dict(plugins=['celery_jobs',
-                             'climos_test',
-                             'user_quota',
-                             'hdfs_assetstore',
-                             'jobs',
-                             'romanesco',
-                             'sparktest']))
+client.put('system/plugins',
+           parameters=dict(plugins=json.dumps(['celery_jobs',
+                                               'climos_test',
+                                               'user_quota',
+                                               'hdfs_assetstore',
+                                               'jobs',
+                                               'romanesco',
+                                               'sparktest'])))
 
 client.put('system/restart')
 
 sleep(30)
 
-put(client,
-    'system/setting',
-    parameters=dict(list=[dict(key='romanesco.require_auth', value=False),
-                          dict(key='romanesco.broker', value=args.broker),
-                          dict(key='romanesco.backend', value=args.broker)]))
+client.put('system/setting',
+           parameters=dict(list=json.dumps([
+               dict(key='romanesco.require_auth', value=False),
+               dict(key='romanesco.broker', value=args.broker),
+               dict(key='romanesco.backend', value=args.broker)])))
 
 hdfs_assetstore_name = 'hdfs'
 if find_assetstore(hdfs_assetstore_name) is None:
-    post(client,
-         'assetstore',
-         parameters=dict(name=hdfs_assetstore_name,
-                         type='hdfs',
-                         path='/girder',
-                         host=args.hdfs_namenode,
-                         user=args.hdfs_user,
-                         port=args.hdfs_port,
-                         webHdfsPort=args.web_hdfs_port))
+    client.post('assetstore',
+                parameters=dict(name=hdfs_assetstore_name,
+                                type='hdfs',
+                                path='/girder',
+                                host=args.hdfs_namenode,
+                                user=args.hdfs_user,
+                                port=str(args.hdfs_port),
+                                webHdfsPort=str(args.web_hdfs_port)))
 
 hdfs_collection_name = 'hdfs'
 if find_collection(hdfs_collection_name) is None:
-    post(client,
-         'collection',
-         parameters=dict(name=hdfs_collection_name,
-                         description='HDFS Collection',
-                         public=False))
+    client.post('collection',
+                parameters=dict(name=hdfs_collection_name,
+                                description='HDFS Collection',
+                                public="false"))
 
 client.put('system/restart')
 
