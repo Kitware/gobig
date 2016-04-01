4920b41a06c637ed2015fdfa6eb72997acab26a3
Omar Padron <omar.padron@kitware.com>
2016-04-01 16:09:59 -0400

    replace logic in aws_credentials module with more gracefully failing logic

--- library/aws_credentials.py
+++ library/aws_credentials.py
@@ -3,7 +3,21 @@
 import os
 import traceback as tb
 
-from ConfigParser import ConfigParser
+try:
+    from configparser import SafeConfigParser as ConfigParser,
+                             NoSectionError, NoOptionError
+except ImportError:
+    from ConfigParser import SafeConfigParser as ConfigParser,
+                             NoSectionError, NoOptionError
+
+def get_profile_value(parser, profile, key, default=None):
+    result = default
+    try:
+        result = parser.get(profile, key)
+    except (NoSectionError, NoOptionError):
+        pass
+
+    return result
 
 def main():
     arg_spec = {
@@ -21,18 +35,24 @@ def main():
     path      = get("path")
     set_facts = get("set_facts")
 
+    default_key_id = None
+    default_secret = None
+
     if path is None:
         path = os.path.join(os.environ.get("HOME", "~"), ".aws", "credentials")
 
-    key_id = os.environ.get("AWS_ACCESS_KEY_ID")
-    secret = os.environ.get("AWS_SECRET_ACCESS_KEY")
-
     try:
-        if not (key_id and secret):
-            parser = ConfigParser()
-            parser.read([os.path.expanduser(path)])
-            key_id = parser.get(profile, "aws_access_key_id")
-            secret = parser.get(profile, "aws_secret_access_key")
+        parser = ConfigParser()
+        parser.read([os.path.expanduser(path)])
+
+        if profile is not None:
+            key_id = get_profile_value(parser, profile, "aws_access_key_id")
+            secret = get_profile_value(parser, profile, "aws_secret_access_key")
+
+        default_key_id = get_profile_value(
+            parser, "default", "aws_access_key_id")
+        default_secret = get_profile_value(
+            parser, "default", "aws_secret_access_key")
     except:
         fail(
             msg="unknown error occured",
@@ -40,20 +60,38 @@ def main():
         )
         raise
 
-    result = {
-        "msg": "AWS credentials successfully set",
-        "changed": False,
-        "access_key_id": key_id,
-        "secret_key": secret,
-    }
+    if key_id is None or secret is None:
+        key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+        secret = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-    if set_facts:
-        result["ansible_facts"] = {
-            "aws_access_key_id": result["access_key_id"],
-            "aws_secret_key": result["secret_key"],
+    if key_id is None or secret is None:
+        key_id = default_key_id
+        secret = default_secret
+
+    if key_id is None or secret is None:
+        result = fail
+        result_data = {
+            "msg": "failed to acquire a suitable set of AWS credentials",
+            "changed": False,
+            "aws_access_key_id": key_id,
+            "aws_secret_key": secret,
+        }
+    else:
+        result = exit
+        result_data = {
+            "msg": "AWS credentials successfully set",
+            "changed": False,
+            "access_key_id": key_id,
+            "secret_key": secret,
         }
 
-    exit(**result)
+    if set_facts:
+        result_data["ansible_facts"] = {
+            "aws_access_key_id": result_data["access_key_id"],
+            "aws_secret_key": result_data["secret_key"],
+        }
+
+    result(**result_data)
 
 
 from ansible.module_utils.basic import *
